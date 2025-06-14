161404bb58e3f53e47d157ac96d4d29229d04e81
Andrew <89681138+Kyushuki@users.noreply.github.com>
2025-06-14 18:04:09 +0300

    Начало декодинга
    Демодуляция. Комплексные числа в строку битов

---
+++ decoder/demodulate.py
@@ -0,0 +1,28 @@
+class Demodulate():
+    """
+    Класс демодуляции комплексных чисел в биты
+    """
+    map = {
+        "00": complex(0,1),
+        "01": complex(-1,0),
+        "10": complex(0,-1),
+        "11": complex(1,0),
+    }
+
+    def demodulate(self,mess: list[complex])-> str:
+        """
+        Метод превращает комплексные числа в строку битов
+
+        Параметры:
+
+        mess: list[complex] список комплексных чисел
+
+        Возвращает - string
+        """
+        res=[]
+        for i in mess:
+            for key, value in self.map.items():
+                if i == value:
+                    res+=key
+        return ''.join(res)
+    
--- encoder/main.py
+++ encoder/main.py
@@ -1,12 +1,12 @@
 from input import Input
 from coder.my_encoder import MyEncoder
 # from coder.coder import Coder
-from modulate import Modulate
+from modulate import QPSKModulate
 
 key = Input()
 coder = MyEncoder()
 # coder = Coder()
-modulator = Modulate()
+modulator = QPSKModulate()
 
 mess = key.input()
 newMess = coder.encode(mess)

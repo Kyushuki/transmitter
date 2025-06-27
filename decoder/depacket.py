6bb29f20556020e9f600dc39bc054529ef2827fc
Andrew <89681138+Kyushuki@users.noreply.github.com>
2025-06-27 21:00:20 +0300

    Ошибка шума, Рабочая тема
    Шум добавил, всё норм расшифровывается при малом шуме

--- decoder/depacket.py
+++ decoder/depacket.py
@@ -10,9 +10,13 @@ class DePacket():
     def depacket(self, mess: str) -> tuple:
         res = ""
         code = ""
-        if mess[0:4] == self.HEADER:
+        if mess[0:4] != self.HEADER:
+            return mess, "error"
+        else:
             res = mess[4:]
             code = res[0:2]
+            if code not in self.codes:
+                return mess, "error"
             res = res[2:]
             return res, self.codes[code]
         # return mess[6:], "basic"
--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class Model:
         self.coder = ""
         self.file = 'my_code.json'
         self.beta = 0.5  # коэфф затухания
+        self.mean = 0
+        self.sigma = 0.1
 
     def import_coder(self, file: str) -> dict:
         # file - название файла .json
@@ -46,19 +48,31 @@ class Model:
         packed_mess = self.packing.pack(byte_mess, code_for_packet)
         complex_mess = self.modulator.modulate_qpsk(packed_mess)
 
+        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='violet', label='эталон')
+
         r = self.start_signal_phase()
-        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='violet')
         complex_mess = [c * r for c in complex_mess]
-        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='red')
+        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='red', label='с поворотом')
+
         complex_mess = [c * self.beta for c in complex_mess]
-        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='blue')
-        plt.grid(True)
-        plt.show()
+        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='blue', label='с затуханием')
+
+        for i in range(len(complex_mess)):
+            complex_mess[i] += np.random.normal(self.mean, self.sigma) + 1j * np.random.normal(self.mean, self.sigma)
+        plt.scatter(np.array(complex_mess).real, np.array(complex_mess).imag, color='green', label='с шумом', marker='*')
+
         return complex_mess
 
     def receiver(self, complex_mess: list[complex]):
+        counter = 0
         packed_mess = self.demodulator.demodulate_qpsk(complex_mess)
         byte_mess, coder = self.depacking.depacket(packed_mess)
+        if coder == "error":
+            while coder == "error":
+                packed_mess = self.demodulator.demodulate_qpsk(complex_mess)
+                byte_mess, coder = self.depacking.depacket(packed_mess)
+                if counter >= 100:
+                    return "ОШИБКА, неудалось расшифровать сообщение"
         if coder == "mycode":
             from decoder.coder.my_decoder import MyDecoder
             code = self.import_coder(self.file)
@@ -75,4 +89,10 @@ if __name__ == "__main__":
     a = model.emitter()
     print(a)
     b = model.receiver(a)
-    print(b)
+    print(f"Получено сообщение:\n {b}")
+    plt.grid(True, linestyle="--")
+    plt.axhline(0, 0, color='black', linewidth=0.4)
+    plt.axvline(0, 0, color='black', linewidth=0.4)
+    plt.title("Созвездие сигнала от передатчика")
+    plt.legend()
+    plt.show()
